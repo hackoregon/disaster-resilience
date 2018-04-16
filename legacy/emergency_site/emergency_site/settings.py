4ad9fde01b9febc6ef3ea79f00fe4dcfc31df8dc
nam20485 <nmiller217@gmail.com>
2018-04-15 19:20:31 -0700

    connect to legacy postgres DB

--- legacy/emergency_site/emergency_site/settings.py
+++ legacy/emergency_site/emergency_site/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import credentials
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,10 +76,18 @@ WSGI_APPLICATION = 'emergency_site.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #},
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': credentials.DB_HOST,
+        "PORT": credentials.DB_PORT,
+        'NAME': credentials.DB_DATABASENAME,
+        'USER': credentials.DB_USERNAME,
+        'PASSWORD': credentials.DB_PASSWORD,        
+    },
 }
 
 
