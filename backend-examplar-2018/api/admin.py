8dcecfe18c1dc2bad2633e0fac933cd896a2a2b3
nam20485 <nmiller217@gmail.com>
2018-04-24 15:21:53 -0700

    add preexisting models auto-generated from schema

--- backend-examplar-2018/api/admin.py
+++ backend-examplar-2018/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from api import preexisting_models
+
+# Register your models here.
+
+admin.site.register(preexisting_models.NeighborhoodUnits)
+
 # Register your models here.
---
+++ backend-examplar-2018/api/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+from api.preexisting_models import NeighborhoodUnits
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+class NeighborhoodUnitsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = NeighborhoodUnits
+        fields = '__all__'
