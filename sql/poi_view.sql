63f927917e918a98169d8c849e50e8160b90efc8
Moss Drake <mxmossman@gmail.com>
2018-06-08 11:11:29 -0700

    Update poi_view.sql
    Convert geometry to a point

--- sql/poi_view.sql
+++ sql/poi_view.sql
@@ -1,55 +1,57 @@
--- View: public.POI_view
+-- View: public."POI_view"
 
--- DROP VIEW public.POI_view;
+-- DROP VIEW public."POI_view";
 
-CREATE OR REPLACE VIEW public.POI_view AS
-SELECT 
-  ogc_fid,
-  'BEECN' as type, 
-  hub_name as description_txt,
-  site_name as description2_txt,
-  location as address, 
-  'Portland' as city, 
-  'OR' as state, 
-  '' as zipcode, 
-  wkb_geometry
-FROM public.basic_earthquake_emergency_communication_node_beecn_locations
-union
-SELECT
- ogc_fid, 
- 'Fire Station' as type, 
- station as description_txt,
- district as description2_txt, 
- address,
-  city, 
-  'OR' as state, 
-  '' as zipcode, 
- wkb_geometry
-FROM public.fire_sta
-union
-SELECT
-  ogc_fid, 
- 'Hospital' as type, 
-  name as description_txt, 
-  '' as description2_txt,
-  address, 
-  city, 
-  'OR' as state, 
-  '' as zipcode, 
-  wkb_geometry
-FROM public.hospital
-union
-SELECT 
-  ogc_fid, 
- 'School' as type, 
-  name as description_txt, 
-  level_name as description2_txt, 
-  address,
-  city, 
-  state, 
-  zipcode, 
-  wkb_geometry
-FROM public.schools;
+CREATE OR REPLACE VIEW public."POI_view" AS 
+ SELECT basic_earthquake_emergency_communication_node_beecn_locations.ogc_fid,
+    'BEECN'::text AS type,
+    basic_earthquake_emergency_communication_node_beecn_locations.hub_name AS description_txt,
+    basic_earthquake_emergency_communication_node_beecn_locations.site_name AS description2_txt,
+    basic_earthquake_emergency_communication_node_beecn_locations.location AS address,
+    'Portland'::character varying AS city,
+    'OR'::text AS state,
+    ''::text AS zipcode,
+    (ST_Dump(ST_Transform (basic_earthquake_emergency_communication_node_beecn_locations.wkb_geometry, 4326))).geom as wkb_geometry
+--    basic_earthquake_emergency_communication_node_beecn_locations.wkb_geometry
+   FROM basic_earthquake_emergency_communication_node_beecn_locations
+UNION
+ SELECT fire_sta.ogc_fid,
+    'Fire Station'::text AS type,
+    fire_sta.station AS description_txt,
+    fire_sta.district AS description2_txt,
+    fire_sta.address,
+    fire_sta.city,
+    'OR'::text AS state,
+    ''::text AS zipcode,
+    (ST_Dump(ST_Transform (fire_sta.wkb_geometry, 4326))).geom as wkb_geometry
+--    fire_sta.wkb_geometry
+   FROM fire_sta
+UNION
+ SELECT hospital.ogc_fid,
+    'Hospital'::text AS type,
+    hospital.name AS description_txt,
+    ''::character varying AS description2_txt,
+    hospital.address,
+    hospital.city,
+    'OR'::text AS state,
+    ''::text AS zipcode,
+    (ST_Dump(ST_Transform (hospital.wkb_geometry, 4326))).geom as wkb_geometry
+--     hospital.wkb_geometry
+   FROM hospital
+UNION
+ SELECT schools.ogc_fid,
+    'School'::text AS type,
+    schools.name AS description_txt,
+    schools.level_name AS description2_txt,
+    schools.address,
+    schools.city,
+    schools.state,
+    schools.zipcode,
+    (ST_Dump(ST_Transform (wkb_geometry, 4326))).geom as wkb_geometry
+--    schools.wkb_geometry
+   FROM schools;
 
-ALTER TABLE public.POI_view
+ALTER TABLE public."POI_view"
   OWNER TO "disaster-resilience";
+GRANT ALL ON TABLE public."POI_view" TO "disaster-resilience";
+GRANT SELECT ON TABLE public."POI_view" TO "disaster-resilience-readonly";
