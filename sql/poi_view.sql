1c51ffcedddf47458797587a0df7288e6ab819e9
Moss Drake <mxmossman@gmail.com>
2018-06-09 12:36:26 -0700

    Update poi_view.sql
    add compound field as primary key: pk_id contains both type and id

--- sql/poi_view.sql
+++ sql/poi_view.sql
@@ -1,9 +1,11 @@
 -- View: public."POI_view"
 
--- DROP VIEW public."POI_view";
+DROP VIEW public."POI_view";
 
 CREATE OR REPLACE VIEW public."POI_view" AS 
- SELECT basic_earthquake_emergency_communication_node_beecn_locations.ogc_fid,
+ SELECT
+    'BEECN'||basic_earthquake_emergency_communication_node_beecn_locations.ogc_fid as pk_id,
+    basic_earthquake_emergency_communication_node_beecn_locations.ogc_fid,
     'BEECN'::text AS type,
     basic_earthquake_emergency_communication_node_beecn_locations.hub_name AS description_txt,
     basic_earthquake_emergency_communication_node_beecn_locations.site_name AS description2_txt,
@@ -11,11 +13,12 @@ CREATE OR REPLACE VIEW public."POI_view" AS
     'Portland'::character varying AS city,
     'OR'::text AS state,
     ''::text AS zipcode,
-    (ST_Dump(ST_Transform (basic_earthquake_emergency_communication_node_beecn_locations.wkb_geometry, 4326))).geom as wkb_geometry
---    basic_earthquake_emergency_communication_node_beecn_locations.wkb_geometry
+    (st_dump(st_transform(basic_earthquake_emergency_communication_node_beecn_locations.wkb_geometry, 4326))).geom AS wkb_geometry
    FROM basic_earthquake_emergency_communication_node_beecn_locations
 UNION
- SELECT fire_sta.ogc_fid,
+ SELECT
+    'Fire Station'||fire_sta.ogc_fid as pk_id,
+    fire_sta.ogc_fid,
     'Fire Station'::text AS type,
     fire_sta.station AS description_txt,
     fire_sta.district AS description2_txt,
@@ -23,11 +26,12 @@ UNION
     fire_sta.city,
     'OR'::text AS state,
     ''::text AS zipcode,
-    (ST_Dump(ST_Transform (fire_sta.wkb_geometry, 4326))).geom as wkb_geometry
---    fire_sta.wkb_geometry
+    (st_dump(st_transform(fire_sta.wkb_geometry, 4326))).geom AS wkb_geometry
    FROM fire_sta
 UNION
- SELECT hospital.ogc_fid,
+ SELECT
+    'Hospital'||hospital.ogc_fid as pk_id,
+   hospital.ogc_fid,
     'Hospital'::text AS type,
     hospital.name AS description_txt,
     ''::character varying AS description2_txt,
@@ -35,11 +39,12 @@ UNION
     hospital.city,
     'OR'::text AS state,
     ''::text AS zipcode,
-    (ST_Dump(ST_Transform (hospital.wkb_geometry, 4326))).geom as wkb_geometry
---     hospital.wkb_geometry
+    (st_dump(st_transform(hospital.wkb_geometry, 4326))).geom AS wkb_geometry
    FROM hospital
 UNION
- SELECT schools.ogc_fid,
+ SELECT
+    'School'||schools.ogc_fid as pk_id,
+    schools.ogc_fid,
     'School'::text AS type,
     schools.name AS description_txt,
     schools.level_name AS description2_txt,
@@ -47,8 +52,7 @@ UNION
     schools.city,
     schools.state,
     schools.zipcode,
-    (ST_Dump(ST_Transform (wkb_geometry, 4326))).geom as wkb_geometry
---    schools.wkb_geometry
+    (st_dump(st_transform(schools.wkb_geometry, 4326))).geom AS wkb_geometry
    FROM schools;
 
 ALTER TABLE public."POI_view"
