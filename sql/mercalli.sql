4da73abf62be6a1071ee029e46b3cc4ca305da47
Moss Drake <mxmossman@gmail.com>
2018-06-10 14:30:53 -0700

    Create mercalli.sql
    Create modified mercalli index lookup table

---
+++ sql/mercalli.sql
@@ -0,0 +1,19 @@
+-- Table: public.mercalli
+
+-- DROP TABLE public.mercalli;
+
+CREATE TABLE public.mercalli
+(
+  id integer NOT NULL,
+  value_txt character varying(10),
+  detail_txt character varying(255),
+  color_ind character varying(10),
+  magnitude_txt character varying(25),
+  description_txt character varying,
+  CONSTRAINT mercalli_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.mercalli
+  OWNER TO "disaster-resilience";
