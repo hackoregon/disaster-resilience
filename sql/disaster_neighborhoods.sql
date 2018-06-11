3af5dbb335281ab6ab8ae10fcfb0ed602a0465c2
Moss Drake <mxmossman@gmail.com>
2018-06-10 20:57:21 -0700

    Update disaster_neighborhoods.sql
    Update with changes from Fred
    "I attached the wrong csv file to my previous email, and copied the wrong one to google drive, so the db doesn't include the
    column we need for the sandbox and card 1, PGD_total_wet_mean_DI."

--- sql/disaster_neighborhoods.sql
+++ sql/disaster_neighborhoods.sql
@@ -4,14 +4,8 @@ drop table public.disaster_neighborhoods;
 
 create table public.disaster_neighborhoods(
 	id integer primary key,
-	fid integer,
 	name varchar(255),
-	area varchar(255),
-	sqmile varchar(255),
-	area_recal varchar(255),
-	perim varchar(255),
-	isop varchar(255),
-	wkb_geometry geometry,
+	wkb_geometrygeometry,
 	pgv_site_count varchar(255),
 	pgv_site_max varchar(255),
 	pgv_site_mean varchar(255),
@@ -32,6 +26,7 @@ create table public.disaster_neighborhoods(
 	pgd_liquefaction_wet_mean varchar(255),
 	pgd_liquefaction_wet_min varchar(255),
 	pgd_liquefaction_wet_std varchar(255),
+	pgd_total_wet_mean varchar(255),
 	pgv_site_min_mmi varchar(255),
 	pgv_site_max_mmi varchar(255),
 	pgv_site_mean_mmi varchar(255),
@@ -43,7 +38,8 @@ create table public.disaster_neighborhoods(
 	pgd_landslide_wet_mean_di varchar(255),
 	pgd_liquefaction_wet_min_di varchar(255),
 	pgd_liquefaction_wet_max_di varchar(255),
-	pgd_liquefaction_wet_mean_di varchar(255)
+	pgd_liquefaction_wet_mean_di varchar(255),
+	pgd_total_wet_mean_di varchar(255)
 );
 
 alter table public.disaster_neighborhoods
