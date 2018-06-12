1203b10f740cfc4a1f3c8e7946419c1c422b8f03
Moss Drake <mxmossman@gmail.com>
2018-06-11 23:17:20 -0700

    Initial commit - disaster_neighborhood_grids
    SQL and csv data for the extract of DOGAMI raster. From Fred Pearce.

---
+++ sql/disaster_neighborhood_grid.sql
@@ -0,0 +1,51 @@
+-- Table: public.disaster_neighborhood_grid
+
+-- DROP TABLE public.disaster_neighborhood_grid;
+
+CREATE TABLE public.disaster_neighborhood_grid
+(
+  id integer NOT NULL,
+  wkb_geometry geometry,
+  pgv_site_count character varying(255),
+  pgv_site_max character varying(255),
+  pgv_site_mean character varying(255),
+  pgv_site_min character varying(255),
+  pgv_site_std character varying(255),
+  pgd_landslide_dry_count character varying(255),
+  pgd_landslide_dry_max character varying(255),
+  pgd_landslide_dry_mean character varying(255),
+  pgd_landslide_dry_min character varying(255),
+  pgd_landslide_dry_std character varying(255),
+  pgd_landslide_wet_count character varying(255),
+  pgd_landslide_wet_max character varying(255),
+  pgd_landslide_wet_mean character varying(255),
+  pgd_landslide_wet_min character varying(255),
+  pgd_landslide_wet_std character varying(255),
+  pgd_liquefaction_wet_count character varying(255),
+  pgd_liquefaction_wet_max character varying(255),
+  pgd_liquefaction_wet_mean character varying(255),
+  pgd_liquefaction_wet_min character varying(255),
+  pgd_liquefaction_wet_std character varying(255),
+  pgd_total_wet_mean character varying(255),
+  pgv_site_min_mmi integer,
+  pgv_site_max_mmi integer,
+  pgv_site_mean_mmi integer,
+  pgd_landslide_dry_min_di character varying(255),
+  pgd_landslide_dry_max_di character varying(255),
+  pgd_landslide_dry_mean_di character varying(255),
+  pgd_landslide_wet_min_di character varying(255),
+  pgd_landslide_wet_max_di character varying(255),
+  pgd_landslide_wet_mean_di character varying(255),
+  pgd_liquefaction_wet_min_di character varying(255),
+  pgd_liquefaction_wet_max_di character varying(255),
+  pgd_liquefaction_wet_mean_di character varying(255),
+  pgd_total_wet_mean_di character varying(255),
+  CONSTRAINT disaster_neighborhood_grid_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.disaster_neighborhood_grid
+  OWNER TO "disaster-resilience";
+GRANT ALL ON TABLE public.disaster_neighborhood_grid TO "disaster-resilience";
+GRANT SELECT ON TABLE public.disaster_neighborhood_grid TO "disaster-resilience-readonly";
